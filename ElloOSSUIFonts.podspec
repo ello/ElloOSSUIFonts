56b5891d2d09dd8728db5c70944ab3de21bc4398
Colin T.A. Gray <colinta@gmail.com>
2017-06-16 21:30:29 +0100

    add 'black' fonts

--- ElloOSSUIFonts.podspec
+++ ElloOSSUIFonts.podspec
@@ -1,11 +1,11 @@
 Pod::Spec.new do |s|
   s.name             = 'ElloOSSUIFonts'
-  s.version          = '2.0.0'
+  s.version          = '2.1.0'
   s.summary          = 'The Open Source fonts for Ello apps.'
   s.homepage         = 'https://github.com/ello/ElloOSSUIFonts'
   s.license          = 'MIT'
-  s.authors          = 'Ello', 'Sean Dougherty'
-  s.source           = { git: 'https://github.com/ello/ElloOSSUIFonts.git', tag: s.version.to_s }
+  s.authors          = 'Ello'
+  s.source           = { git: 'git@github.com:ello/ElloOSSUIFonts.git', tag: s.version }
 
   s.platform = :ios, '8.0'
   s.requires_arc = true
--- Pod/Classes/ElloOSSUIFonts.swift
+++ Pod/Classes/ElloOSSUIFonts.swift
@@ -3,21 +3,15 @@ extension UIFont {
     public class func loadFonts() {}
 
     public class func defaultFont(_ size: CGFloat = 14) -> UIFont { return UIFont.systemFont(ofSize: size) }
-
     public class func defaultBoldFont(_ size: CGFloat = 14) -> UIFont { return UIFont.boldSystemFont(ofSize: size) }
-
     public class func defaultItalicFont(_ size: CGFloat = 14) -> UIFont { return UIFont.italicSystemFont(ofSize: size) }
-
     public class func regularFont(_ size: CGFloat = 14) -> UIFont { return UIFont.systemFont(ofSize: size) }
-
     public class func regularBoldFont(_ size: CGFloat = 14) -> UIFont { return UIFont.boldSystemFont(ofSize: size) }
-
+    public class func regularBlackFont(_ size: CGFloat = 14) -> UIFont { return UIFont.boldSystemFont(ofSize: size) }
+    public class func regularBlackItalicFont(_ size: CGFloat = 14) -> UIFont { return UIFont.italicSystemFont(ofSize: size) }
     public class func editorFont(_ size: CGFloat = 14) -> UIFont { return UIFont.systemFont(ofSize: size) }
-
     public class func editorItalicFont(_ size: CGFloat = 14) -> UIFont { return UIFont.italicSystemFont(ofSize: size) }
-
     public class func editorBoldFont(_ size: CGFloat = 14) -> UIFont { return UIFont.boldSystemFont(ofSize: size) }
-
     public class func editorBoldItalicFont(_ size: CGFloat = 14) -> UIFont {
         let descriptor = UIFont.systemFont(ofSize: size).fontDescriptor.withSymbolicTraits([.traitBold, .traitItalic])
         return UIFont(descriptor: descriptor!, size: size)
